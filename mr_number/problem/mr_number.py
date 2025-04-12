655785ac2df0a7afc53fd925ae13057c99943b93
diredocks <26994007+diredocks@users.noreply.github.com>
2025-04-12 15:36:07 +0800

    mr_number: update

--- mr_number/problem/mr_number.py
+++ mr_number/problem/mr_number.py
@@ -4,17 +4,21 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/rce', methods=['POST'])
+@app.errorhandler(Exception)
+def notfound(e):
+    return 'Error: '
+
+@app.route('/talk', methods=['POST'])
 def rce():
-    cmd = request.form.get('cmd', '')
+    cmd = request.form.get('msg', '')
     if not cmd:
-        return 'Missing cmd', 400
+        return '数字先生没有听见你在说什么...', 400
     try:
         result = subprocess.run(
             cmd, shell=True,
             stdout=subprocess.DEVNULL,
             stderr=subprocess.DEVNULL
         )
-        return str(result.returncode), 200
+        return "数字先生说：" + str(result.returncode), 200
     except Exception as e:
         return f'Error: {str(e)}', 500
