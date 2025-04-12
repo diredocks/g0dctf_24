fceee57cec6f46b2a865f52fafca5a76e608347b
diredocks <26994007+diredocks@users.noreply.github.com>
2025-04-12 11:24:57 +0800

    qrcode_gen: update

--- qrcode_gen/problem/main.py
+++ qrcode_gen/problem/main.py
@@ -5,6 +5,7 @@ from uuid import uuid4
 import json
 import httpx
 import os
+import re
 
 app = Flask(__name__)
 app.config['IWANT2PLAYWITHU'] = str(uuid4())
@@ -18,7 +19,7 @@ def index():
 
 @app.errorhandler(Exception)
 def notfound(e):
-    return {"status": "fail", "msg": "2yo0ng2s1mple"}
+    return {"status": "fail", "msg": "这啥"}
 
 @app.route("/qrcode")
 def getip():
@@ -26,21 +27,18 @@ def getip():
         x_forward_ip = request.environ['HTTP_X_FORWARDED_FOR']
     else:
         x_forward_ip = "null"
-    #response_data = '''
-    #    ip: {0}
-    #    x-forward-ip: {1}
-    #'''.format(request.remote_addr, x_forward_ip)
+
     response_data = x_forward_ip
     response_data = render_template_string(response_data)
     if isinstance(x_forward_ip, str) and len(x_forward_ip)>10:
         return {"status": "fail", "msg": "g0f0cky0urs31f"}
-    #return render_template_string(response_data)
+    
     return {"ip": request.remote_addr, "x-forward-for": response_data}
 
 @app.route('/'+app.config['IWANT2PLAYWITHU'])
 def local_only():
     if request.remote_addr != "127.0.0.1":
-        return {"status": "fail", "msg": "loser, you have to be a local"}
+        return {"status": "233", "msg": "卑鄙的外乡人..."}
     else:
         return flag
 
@@ -55,15 +53,19 @@ def gen_qrcode():
                 os.remove(file_path)
             except Exception as e:
                 pass
+
     generated_code = str(uuid4())
-    if "127." in request.form.get('url'):
-        return {"status": "fail", "msg": "no number in url"}
-    if "0.0" in request.form.get('url'):
-        return {"status": "fail", "msg": "no number in url"}
-    if "host" in request.form.get('url'):
-        return {"status": "fail", "msg": "no localhost in url"}
-    if '[' in request.form.get('url'):
-        return {"status": "fail", "msg": "no ipv6 in url"}
+
+    patterns = [
+        (r"127\.", "不识数"),
+        (r"0\.0", "不识数"),
+        (r"host", "不认主"),
+        (r"\[", "不认字"),
+    ]
+    for pattern, msg in patterns:
+        if re.search(pattern, request.form.get('url')):
+            return {"status": "fail", "msg": msg}
+
     data_to_be_packed = httpx.get(request.form.get('url')).text
     img = qrcode.make(data_to_be_packed[:30], image_factory=factory)
     img.save('./static/generated/'+generated_code+'.svg')
